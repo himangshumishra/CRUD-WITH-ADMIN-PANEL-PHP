676aa0c7418086ebcc9a1e3046ec5c558d0ec441
Himangshu Mishra <115614942+himangshumishra@users.noreply.github.com>
2024-02-25 17:49:27 +0530

    updt

--- view_post.php
+++ view_post.php
@@ -18,7 +18,6 @@ if (isset($_GET['id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $post['title']; ?></title>
-    <!-- Including Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="bg-gray-300">
