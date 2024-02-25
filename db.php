2425078d28e759df2b1160754b5b5b0329d17fe3
Himangshu Mishra <115614942+himangshumishra@users.noreply.github.com>
2024-02-25 15:48:21 +0530

    starter

--- db.php
+++ db.php
@@ -9,6 +9,6 @@ $conn = mysqli_connect($servername, $username, $password, $dbname,"3307");
 
 // Check connection
 if ($conn->connect_error) {
-    // die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
